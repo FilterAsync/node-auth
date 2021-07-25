6b02acc369d055877c2696a49f3d8c12e3ea3e8f
Filter Async <guestpogu129@gmail.com>
2021-07-25 19:56:34 +0700

    Update session.mjs

--- config/session.mjs
+++ config/session.mjs
@@ -9,6 +9,11 @@ if (ENV.NODE_ENV !== "production") {
 export const sessionOptions = {
 	secret: ENV.SESSION_SECRET,
 	name: ENV.SESSION_NAME,
+	cookie: {
+		sameSite: true,
+		maxAge: +ENV.SESSION_TIMEOUT,
+		secure: "auto",
+	},
 	resave: false,
 	saveUninitialized: true,
 };
