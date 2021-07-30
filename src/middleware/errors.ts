ef0672054e6b675410397194c5a3438bd15ad2c0
Filter Async <guestpogu129@gmail.com>
2021-07-30 14:56:31 +0700

    Update errors

--- src/middleware/errors.ts
+++ src/middleware/errors.ts
@@ -15,15 +15,8 @@ export const decodeUriError: RequestHandler = (req, res, next) => {
 };
 
 export const internalServerError: ErrorRequestHandler = (err, _, res, next) => {
-	if (!err.status) {
-		console.error(err.stack);
-	}
-	const statusCode = err.statusCode || 500;
-	const message = err.message || "Internal Server Error";
+	console.error(err.stack);
 
-	res.status(statusCode).json({
-		status: statusCode,
-		message: message,
-	});
+	res.status(500).render("500");
 	next();
 };
